9b96dedbee72d135e7700359787751c3a1735250
kelvingakuo <kelvingakuo@gmail.com>
2018-05-02 06:06:13 +0300

    Added bot with API access

--- APIBot.py
+++ APIBot.py
@@ -2,17 +2,38 @@ from twython import Twython
 import sys
 import csv
 import json
+import time
 
 
-def collectTweets(searchTerm, filename, api):
-	searchTerm = '#' + str(searchTerm)
-	tweetObjects = {}
-	tweetObjects['TweetData']=[]
+def collectTweets(searchTerm, filename, api, since=None, obj={}):
+	theTerm = '#' + str(searchTerm) + '-filter:retweets AND -filter:replies'
+	ids = []
+
+	if(len(obj)==0):
+		tweetObjects = {}
+		tweetObjects['TweetData']=[]
+	
+	else:
+		tweetObjects = obj
+		tweetObjects['TweetData']=obj['TweetData']
+
+
 
 	with open(filename, 'a') as dump:
-		for tweet in api.search(q=searchTerm, lang='en', result_type='recent', count=100, tweet_mode='extended')['statuses']:
+		for tweet in api.search(q=theTerm, lang='en', result_type='mixed', count=100, tweet_mode='extended', since_id=since)['statuses']:
+			ids.append(tweet['id'])
 			tweetObjects['TweetData'].append(tweet)
-		json.dump(tweetObjects, dump)
+			time.sleep(5)
+		
+		if(len(ids)==0):
+			json.dump(tweetObjects, dump)
+			print("Finished. {} tweets collected".format(len(tweetObjects['TweetData'])))
+			return 
+		else:
+			sorte = sorted(ids)
+			highest = sorte[0]
+			collectTweets(searchTerm, filename, api, highest, tweetObjects)
+
 
 
 consumer_key = ''
@@ -24,5 +45,6 @@ access_token_secret = ''
 api = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
 	
 collectTweets(sys.argv[1], sys.argv[2], api)
+
 
 
